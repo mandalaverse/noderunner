210c068f0adfdff7dfa58570eab1e0af919497c0
bakon <bakon@bakon.dev>
2022-06-28 21:36:21 -0500

    mongodb first metadata test

--- ouraApi/server/typescript/src/methods/getMetaData.ts
+++ ouraApi/server/typescript/src/methods/getMetaData.ts
@@ -1,7 +1,27 @@
 import { GetMetaData } from "../generated-typings";
+import { MongoClient } from 'mongodb';
 
 const getMetaData: GetMetaData = (policyID, assetName) => {
-  return Promise.resolve();
+  return Promise.resolve(
+    mongoDbGetAsset(policyID, assetName)
+  );
+};
+
+const mongoDbGetAsset: GetMetaData = async (policyID, assetName) => {
+  // Connection URL
+  const url = 'mongodb://localhost:27017';
+  const client = new MongoClient(url);
+  const dbName = 'nftmetadata';
+
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('testnet');
+
+  const findResult: any = await collection.find({}).toArray();
+  console.log('Found documents =>', findResult);
+
+  return(findResult);
 };
 
 export default getMetaData;
