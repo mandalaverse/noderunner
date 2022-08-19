eed5c5569b9570c05e49ad8514d6a0b3991c97f2
bakon <bakon@bakon.dev>
2022-08-18 22:53:09 -0500

    updates to volumes

--- setupContainers.sh
+++ setupContainers.sh
@@ -6,6 +6,5 @@ git clone https://github.com/CardanoSolutions/ogmios
 docker build -t cnode-ogmios-kupo . &&
 
 echo "To start the docker container please run the following command: "
-echo "docker run -itd --name cardano-node-ogmios -p 1337:1337 -p 1442:1442 -v cardano-node-db:/db -v kupo-db:/kupodb cnode-ogmios-kupo:latest"
+echo "docker run -itd --name cnode-ogmios-kupo -p 1337:1337 -p 1442:1442 -v cardano-node-db:/db -v kupo-db:/kupodb cnode-ogmios-kupo:latest"
 echo "If you want to see your services logs you can use command: docker logs -f cnode-ogmios-kupo:latest"
-
