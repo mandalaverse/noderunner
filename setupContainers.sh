3319e983b1b7680998156a42f7b4eea5e63c526c
bakon <bakon@bakon.dev>
2022-09-29 22:57:32 -0500

    fixes

--- setupContainers.sh
+++ setupContainers.sh
@@ -28,18 +28,15 @@ ogmios () {
 	git clone https://github.com/CardanoSolutions/ogmios &&
 	cd ogmios &&
 	docker build -t cardano-node-ogmios . &&
-	echo "docker run -it --name cardano-node-ogmios -p 1337:1337 -v cardano-node-db:/db -v cardano-node-config:/config/cardano-node/config.json -v cardano-node-ipc:/ipc/node.socket cardano-node-ogmios"
+	echo "docker run -itd --name cardano-node-ogmios -p 1337:1337 -v cardano-node-db:/db -v cardano-node-config:/config/cardano-node -v cardano-node-ipc:/ipc cardano-node-ogmios"
 }
 
 kupo () {
 	git clone https://github.com/CardanoSolutions/kupo &&
 	cd kupo &&
 	docker build -t kupo . &&
-	exho "docker run -it --name kupo p 1442:1442 -v kupo-db:/db -v cardano-node-ipc:/ipc/node.socket -v cardano-node-config:/config/cardano-node/config.json kupo --node-socket /ipc/node.socket --node-config /config/cardano-node/config.json --host 0.0.0.0 --workdir /db"
+	exho "docker run -itd --name kupo p 1442:1442 -v kupo-db:/db -v cardano-node-ipc:/ipc -v cardano-node-config:/config/cardano-node kupo --node-socket /ipc/node.socket --node-config /config/cardano-node/config.json --host 0.0.0.0 --workdir /db"
 }
 
 welcome
 menu
-
-# docker run -it --name cardano-node-ogmios -p 1337:1337 -v cardano-node-db:/db -v cardano-node-config:/config/cardano-node/config.json -v cardano-node-ipc:/ipc/node.socket cardano-node-ogmios
-# docker run -it --name kupo 				-p 1442:1442 -v kupo-db:/db 		-v cardano-node-ipc:/ipc/node.socket 					-v cardano-node-config:/config/cardano-node/config.json kupo --node-socket /ipc/node.socket --node-config /config/cardano-node/config.json --host 0.0.0.0 --workdir /db
