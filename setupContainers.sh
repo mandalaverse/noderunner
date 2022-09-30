9e45d6c0b72c6a4b6cb9583f33568362eeb561e2
bakon <bakon@bakon.dev>
2022-09-29 22:28:22 -0500

    new menus and wowz

--- setupContainers.sh
+++ setupContainers.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
-git clone https://github.com/CardanoSolutions/kupo
-git clone https://github.com/CardanoSolutions/ogmios
+welcome () {
+	echo "WELCOME TO NODERUNNER"
+	echo "Your Cardano Stack Helper"
+}
 
-docker build -t cnode-ogmios-kupo . &&
+menu () {
+	items=(1 "Spinup Cardano Node with Ogmios Docker container"
+       	2 "Spinup Kupo Docker Container")
 
-echo "To start the docker container please run the following command: "
-echo "docker run -itd --name cnode-ogmios-kupo -p 1337:1337 -p 1442:1442 -v cardano-node-db:/db -v kupo-db:/kupodb cnode-ogmios-kupo:latest"
-echo "If you want to see your services logs you can use command: docker logs -f cnode-ogmios-kupo:latest"
+	while choice=$(dialog --title "$TITLE" \
+                 --menu "Please select" 10 40 3 "${items[@]}" \
+                 2>&1 >/dev/tty)
+    do
+    case $choice in
+        1) ;; # some action on 1 
+        2) ;; # some action on 2
+        *) ;; # some action on other
+    esac
+	done
+	clear # clear after user pressed Cancel
+}
+
+ogmios () {
+	git clone https://github.com/CardanoSolutions/ogmios &&
+	cd ogmios &&
+	docker build -t cardano-node-ogmios . &&
+	echo "docker run -it --name cardano-node-ogmios -p 1337:1337 -v cardano-node-db:/db -v cardano-node-config:/config/cardano-node/config.json -v cardano-node-ipc:/ipc/node.socket cardano-node-ogmios"
+}
+
+kupo () {
+	git clone https://github.com/CardanoSolutions/kupo &&
+	cd kupo &&
+	docker build -t kupo . &&
+	exho "docker run -it --name kupo p 1442:1442 -v kupo-db:/db -v cardano-node-ipc:/ipc/node.socket -v cardano-node-config:/config/cardano-node/config.json kupo --node-socket /ipc/node.socket --node-config /config/cardano-node/config.json --host 0.0.0.0 --workdir /db"
+}
+
+welcome
+menu
+
+# docker run -it --name cardano-node-ogmios -p 1337:1337 -v cardano-node-db:/db -v cardano-node-config:/config/cardano-node/config.json -v cardano-node-ipc:/ipc/node.socket cardano-node-ogmios
+# docker run -it --name kupo 				-p 1442:1442 -v kupo-db:/db 		-v cardano-node-ipc:/ipc/node.socket 					-v cardano-node-config:/config/cardano-node/config.json kupo --node-socket /ipc/node.socket --node-config /config/cardano-node/config.json --host 0.0.0.0 --workdir /db
